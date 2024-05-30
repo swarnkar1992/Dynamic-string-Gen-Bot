bb4b6d9d1dd3b90d7eb25ca23ff864258411bc74
dynamic soni <154851547+swarnkar1992@users.noreply.github.com>
2024-05-30 17:01:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 5329331409))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/dynamicsupportgroup")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/dynamicsupportgroup0")
